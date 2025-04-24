2008a8e92a598bfa13c86b3674449fe4eee1955b
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-04-24 13:14:11 -0300

    .

---
+++ shoppingcart/src/components/Filters.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function Filters() {
+
+    return (
+
+        <label htmlFor="price">price</label>
+
+    )
+}
--- shoppingcart/src/components/Nav.jsx
+++ shoppingcart/src/components/Nav.jsx
@@ -1,11 +1,14 @@
 'use client'
 
+import { Filters } from "./filters"
+
 export function Nav() {
     return (
 
         <div>   
-
+ 
             <h1 style={{ textAlign: 'center', color: 'white', padding: "10px" }}> Carro de compra </h1>
+            <Filters />
 
         </div>
 
