1adcdc29ff6927a96bee8126ae0affefefda4214
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-04-24 13:43:18 -0300

    .

--- shoppingcart/src/components/Filters.jsx
+++ shoppingcart/src/components/Filters.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import "./Filters.css"
 
 export function Filters() {
 
     return (
 
         <section className='filters'>
+            <div className='filter-group'>
+                <label htmlFor="price">Precio</label>
+                <input
+                    type='range'
+                    id='price'
+                    min='0'
+                    max='1000'
+                />
+            </div>
 
-            <label htmlFor="price">price</label>
-            
-            <input type='range' id='price' min='0' max='1000' />
-
+            <div className='filter-group'>
+                <label htmlFor='category'>Categoría</label>
+                <select id="category">
+                    <option value='all'>Todas</option>
+                    <option value='laptops'>Portátiles</option>
+                    <option value='smartphones'>Celulares</option>
+                </select>
+            </div>
         </section>
 
     )
